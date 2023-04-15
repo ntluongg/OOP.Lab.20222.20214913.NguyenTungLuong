770667fad7891a1fd8b532a2127791c23f94f12f
ntluongg <nguyentungluong@gmail.com>
2023-04-15 21:58:45 +0700

    commit

--- Lab01/JavaBasics/src/default_package/ChoosingOption.java
+++ Lab01/JavaBasics/src/default_package/ChoosingOption.java
@@ -6,5 +6,9 @@ public class ChoosingOption {
         int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
         JOptionPane.showMessageDialog(null, "You've chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
+        /* to custom buttoms
+        Object[] options = {"I do", "I don't"};
+        int option = JOptionPane.showOptionDialog(frame, "Do you want to change to the first class ticket?", "Confirm message", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        */
     }
 }
